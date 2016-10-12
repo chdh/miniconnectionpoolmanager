477d8bdf45b3c31dae69a75a2d894cdf5a24d3f9
Christian d'Heureuse <chdh@inventec.ch>
2016-10-12 23:45:04 +0000

    Minor change for compatibility with SQLite JDBC Driver (Xerial-sqlite-jdbc).

--- src/examples/runExample.bat
+++ src/examples/runExample.bat
@@ -10,7 +10,10 @@ rem for H2:
    set "cp=%cp%;%targetDir%\h2.jar"
 
 rem for MS SQL Server:
-   set "cp=%cp%;%targetDir%\sqljdbc4.jar"
-   set "javaParms=%javaParms% -DsaPassword=%sqlServerSaPassword%"
+   rem set "cp=%cp%;%targetDir%\sqljdbc4.jar"
+   rem set "javaParms=%javaParms% -DsaPassword=%sqlServerSaPassword%"
+
+rem for SQLite:
+   rem set "cp=%cp%;%targetDir%\sqlite-jdbc-3.14.2.1.jar"
 
 java -cp "%cp%" %javaParms% %1
